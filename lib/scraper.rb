1764f4082086afc20458068ea3d11400f070372a
William Han Findiesen <william.findiesen@gmail.com>
2020-07-27 04:30:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,12 +8,20 @@ class Scraper
   
   def get_page 
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    
+  end
   
-  #doc.css(".post").each { |post|
-    #course = Course.new
+  def get_courses
+    self.get_page.css(".post")
+  end
+  
+  
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
       course.description = post.css("p").text
-  }
+    end
   end
 
   
